195791b229f62391f3fa16e2c5f97337336c6778
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-04-25 23:42:42 +0700

    dev Update
    - downloaded html page template;
    - inject it into _Layout.csgtml and separeted it apart to logic files;
    - connected separated files inside _Layout.cshtml.

---
+++ PizzaShop/Views/Shared/HeaderPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaShop.Views.Shared
+{
+    public class HeaderPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ PizzaShop/Views/Shared/MetatagsPartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaShop.Views.Shared
+{
+    public class MetatagPartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
