9a624102881385e648f503c9ea66b20b9c504b76
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2023-05-04 23:24:22 +0700

    dev Update(identityPart)
    - added IdentityServer4, connected all services for it;
    - created Configuration class which contains information about clients, resources etc and set these info into program.cs(services).

--- PizzaShop.IdentityPart/PizzaShop.Identity/Program.cs
+++ PizzaShop.IdentityPart/PizzaShop.Identity/Program.cs
@@ -1,5 +1,19 @@
+using IdentityServer4.Models;
+using Microsoft.Extensions.DependencyInjection;
+using PizzaShop.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiResources(Configuration.ApiResources)
+    .AddInMemoryIdentityResources(Configuration.IdentityResources)
+    .AddInMemoryApiScopes(Configuration.ApiScopes)
+    .AddInMemoryClients(Configuration.Clients)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
+
+app.UseRouting();
+app.UseIdentityServer();
 
 app.MapGet("/", () => "Hello World!");
 
